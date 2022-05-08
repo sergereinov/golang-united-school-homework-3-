e3f5d6b1fb428e7f774cff1e3be462e7c68f7b72
sergereinov <47950629+sergereinov@users.noreply.github.com>
2022-05-09 02:22:33 +0300

    Update task03-map.go

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,33 @@
 package homework
 
+import "sort"
+
+type Pair struct {
+	int
+	string
+}
+
+type ByInt []Pair
+
+func (a ByInt) Len() int           { return len(a) }
+func (a ByInt) Less(i, j int) bool { return a[i].int < a[j].int }
+func (a ByInt) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
+
+	//get (int,string) pairs
+	pairs := make([]Pair, 0, len(input))
+	for k, v := range input {
+		pairs = append(pairs, Pair{k, v})
+	}
+
+	//sort pairs by int
+	sort.Sort(ByInt(pairs))
+
+	//copy strings to result
+	result = make([]string, 0, len(pairs))
+	for _, v := range pairs {
+		result = append(result, v.string)
+	}
 	return
 }
